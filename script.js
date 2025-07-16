96630f76cb740419b7812179805307b946c75da2
Chelisbond <141980614+Chelisbond@users.noreply.github.com>
2025-07-16 00:32:01 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+  <label for="pozole_gde">Pozole Gde ($70):</label>
+  <input type="number" id="pozole_gde" min="0">
+  
+  <label for="pozole_ltr">Pozole Litro ($70):</label>
+  <input type="number" id="pozole_ltr" min="0">
+    
+  <label for="pozole_chico">Pozole Chico ($50):</label>
+  <input type="number" id="pozole_chico" min="0">
+  
+  <label for="quesa_con">Quesadillas con carne ($40):</label>
+  <input type="number" id="quesa_con" min="0">
+   
+  <label for="quesa_sin">Quesadillas sin carne ($30):</label>
+  <input type="number" id="quesa_sin" min="0">
+  
+  <label for="sopes_con">Sopes con carne ($20):</label>
+  <input type="number" id="sopes_con" min="0">
+  
+  <label for="sopes_sin">Sopes sin carne ($15):</label>
+  <input type="number" id="sopes_sin" min="0">
+
+  <label for="tacos_dor_sin">Tacos dorados ($12):</label>
+  <input type="number" id="tacos" min="0">
+  
+  <label for="aguas">Aguas frescas ($15):</label>
+  <input type="number" id="aguas" min="0">
+
+  <label for="soda">Refresco ($20):</label>
+  <input type="number" id="soda" min="0">
+
+  <label for="gela">Gelatina ($10):</label>
+  <input type="number" id="gela" min="0">
